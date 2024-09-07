04a6f1ac700227eec1793eb90556ce3211c3bdc6
Mostefai_Mounir <mostefaimounir@gmail.com>
2024-09-07 15:33:41 +0100

    Trying to fix Open graph 4

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,14 +1,14 @@
 import {defineRouting} from 'next-intl/routing';
 import {createSharedPathnamesNavigation} from 'next-intl/navigation';
- 
+
 export const routing = defineRouting({
   // A list of all locales that are supported
   locales: ['fr', 'en', 'ar'],
- 
+
   // Used when no locale matches
   defaultLocale: 'fr'
 });
- 
+
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
 export const {Link, redirect, usePathname, useRouter} =
